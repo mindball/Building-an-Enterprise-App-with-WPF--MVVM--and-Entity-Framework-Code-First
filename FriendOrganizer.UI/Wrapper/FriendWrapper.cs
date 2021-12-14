aee2b41085add2c408db518c80ca32bb3d73c862
mindball <mincho.balaliew@gmail.com>
2021-12-14 10:58:21 +0200

    Implement Wrapper(wrap basic model to be clean) Implement INotifyDataErrorInfo interface logic

---
+++ FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -0,0 +1,121 @@
+﻿using FriendOrganizer.Model;
+using FriendOrganizer.UI.ViewModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Wrapper
+{
+    public class FriendWrapper : ViewModelBase, INotifyDataErrorInfo
+    {
+        private Dictionary<string, List<string>> _errorsByPropertyName;
+        public FriendWrapper(Friend model)
+        {
+            Model = model;
+            _errorsByPropertyName = new Dictionary<string, List<string>>();
+        }
+
+        public Friend Model { get; }
+
+        public int Id { get => Model.Id; }
+
+        public string FirstName
+        {
+            get => Model.FirstName;
+            set
+            {
+                Model.FirstName = value;
+                OnPropertyChanged();
+                ValidateProperty(nameof(FirstName));
+            }
+        }
+
+        public string LastName
+        {
+            get => Model.LastName;
+            set
+            {
+                Model.LastName = value;
+                OnPropertyChanged();
+                ValidateProperty(nameof(LastName));
+            }
+        }
+
+        public string Email
+        {
+            get => Model.Email;
+            set
+            {
+                Model.Email = value;
+                OnPropertyChanged();
+                ValidateProperty(nameof(Email));
+            }
+        }
+
+        public bool HasErrors => _errorsByPropertyName.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return _errorsByPropertyName.ContainsKey(propertyName)
+            ? _errorsByPropertyName[propertyName]
+            : null;
+        }
+
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        private void AddError(string propertyName, string error)
+        {
+            if (!_errorsByPropertyName.ContainsKey(propertyName))
+            {
+                _errorsByPropertyName[propertyName] = new List<string>();
+            }
+
+            var existError =
+                _errorsByPropertyName[propertyName].Contains(error);
+
+            if (!existError)
+            {
+                _errorsByPropertyName[propertyName].Add(error);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        private void ClearErrors(string propertyName)
+        {
+            if (!_errorsByPropertyName.ContainsKey(propertyName))
+            {
+                _errorsByPropertyName.Remove(propertyName);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        private void ValidateProperty(string propertyName)
+        {
+            ClearErrors(propertyName);
+            switch (propertyName)
+            {
+                case nameof(FirstName):
+                    if (string.Equals(FirstName, "Robot", StringComparison.OrdinalIgnoreCase))
+                    {
+                        AddError(propertyName, "Robot are not valid for First name");
+                    }
+                    break;
+                case nameof(LastName):
+
+                    break;
+                case nameof(Email):
+
+                    break;
+            }
+        }
+    }
+}
