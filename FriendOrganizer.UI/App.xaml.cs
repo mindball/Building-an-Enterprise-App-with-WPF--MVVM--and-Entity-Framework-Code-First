26e8f7f492cbec092b9582031c1166b5e371be11
mindball <mincho.balaliew@gmail.com>
2021-12-14 07:41:20 +0200

    DispatcherUnhandledException Event

--- FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer.UI/App.xaml.cs
@@ -9,6 +9,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace FriendOrganizer.UI
 {
@@ -25,5 +26,15 @@ namespace FriendOrganizer.UI
       var mainWindow = container.Resolve<MainWindow>();
       mainWindow.Show();
     }
-  }
+
+        private void Application_DispatcherUnhandledException(
+            object sender, 
+            DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Error" 
+                + Environment.NewLine 
+                + e.Exception.Message, "Unexpected error");
+            e.Handled = true;
+        }
+    }
 }
